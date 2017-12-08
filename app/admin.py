dc5f5b9331a5e8406fe10dfffe5f80b3981ec6f1
SanzharAmirzhan <SanzharAmirzhan@users.noreply.github.com>
2017-12-08 15:31:41 +0600

    nn

--- app/admin.py
+++ app/admin.py
@@ -89,7 +89,7 @@ class UserAdmin(BaseUserAdmin):
         }),
     )
 
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    list_display = ('username', 'id', 'email', 'first_name', 'last_name', 'is_staff')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
     search_fields = ('username', 'first_name', 'last_name', 'email')
     ordering = ('username',)
