6232da83632246ac064dd0bcb5a98932a8129233
SanzharAmirzhan <SanzharAmirzhan@users.noreply.github.com>
2017-12-08 16:22:30 +0600

    nn

--- action/dynamoDB.py
+++ action/dynamoDB.py
@@ -6,12 +6,19 @@ from .env_getter import getVariable
 def postQuestion(answerId, realQuestionId, answer, userId):
     API_ENDPOINT = getVariable('API_GATEWAY_DYNAMODB')
 
-    data = {
-        "questionId": str(answerId),
-        "answer": str(answer),
-        "realQuestionId": str(realQuestionId),
-        "userId": str(userId)
-    }
+    # data = {
+    #     "questionId": str(answerId),
+    #     "answer": str(answer),
+    #     "realQuestionId": str(realQuestionId),
+    #     "userId": str(userId)
+    # }
+
+    data  = {
+    "questionId": "21",
+"answer": "I am Sanzha333333r!",
+"realQuestionId": "4",
+"userId": "8"
+}
 
     print(data)
     headers = {'Content-type': 'application/json', 'Access-Control-Allow-Origin': '*'}
